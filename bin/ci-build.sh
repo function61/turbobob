91963703159c3c9d505b31ac2a1a12d93ec2de56
Joonas Loppi <joonas@joonas.fi>
2018-08-28 17:03:58 +0300

    Fix Bob version

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-curl --fail --location --output bob https://dl.bintray.com/function61/turbobob/dev/bob_linux-amd64
+curl --fail --location --output bob https://dl.bintray.com/function61/turbobob/20180828_1241_08924ed6611f4520/bob_linux-amd64
 
 chmod +x bob
 
