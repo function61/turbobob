4803c6e24bdee45c0a1651bd9034dc98141a35be
Joonas Loppi <joonas@joonas.fi>
2018-08-29 12:28:51 +0300

    Remove need for bin/ci-build.sh

--- bin/ci-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-# Download Bob and hand off the build process to it
-
-curl --fail --location --output bob https://dl.bintray.com/function61/turbobob/20180828_1449_b9d7759cf80f0b4a/bob_linux-amd64
-
-chmod +x bob
-
-./bob build --publish-artefacts
