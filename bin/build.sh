56a4dfa106a33f3b205bae10338917e61ecd8714
Joonas Loppi <joonas@joonas.fi>
2018-10-06 16:49:33 +0300

    Update buildkit

--- bin/build.sh
+++ bin/build.sh
@@ -1,73 +1,9 @@
 #!/bin/bash -eu
 
-run() {
-	fn="$1"
+source /build-common.sh
 
-	echo "# $fn"
+COMPILE_IN_DIRECTORY="cmd/bob"
+BINARY_NAME="bob"
+BINTRAY_PROJECT="function61/turbobob"
 
-	"$fn"
-}
-
-downloadDependencies() {
-	dep ensure
-}
-
-checkFormatting() {
-	# unfortunately we need to list formattable directories because "." would include vendor/
-	local offenders=$(gofmt -l cmd/)
-
-	if [ ! -z "$offenders" ]; then
-		>&2 echo "formatting errors: $offenders"
-		exit 1
-	fi
-}
-
-unitTests() {
-	go test ./...
-}
-
-staticAnalysis() {
-	go vet ./...
-}
-
-buildLinuxArm() {
-	(cd cmd/bob && GOOS=linux GOARCH=arm go build -ldflags "-X main.version=$FRIENDLY_REV_ID" -o ../../rel/bob_linux-arm)
-}
-
-buildLinuxAmd64() {
-	# compile statically so this works on Alpine that doesn't have glibc
-	(cd cmd/bob && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.version=$FRIENDLY_REV_ID -extldflags \"-static\"" -o ../../rel/bob_linux-amd64)
-}
-
-uploadBuildArtefacts() {
-	# the CLI breaks automation unless opt-out..
-	export JFROG_CLI_OFFER_CONFIG=false
-
-	jfrog-cli bt upload \
-		"--user=joonas" \
-		"--key=$BINTRAY_APIKEY" \
-		--publish=true \
-		'rel/*' \
-		"function61/turbobob/main/$FRIENDLY_REV_ID" \
-		"$FRIENDLY_REV_ID/"
-}
-
-rm -rf rel
-mkdir rel
-
-run downloadDependencies
-
-run checkFormatting
-
-run staticAnalysis
-
-run unitTests
-
-run buildLinuxArm
-
-run buildLinuxAmd64
-
-if [ "${PUBLISH_ARTEFACTS:-''}" = "true" ]; then
-	run uploadBuildArtefacts
-fi
-
+standardBuildProcess
