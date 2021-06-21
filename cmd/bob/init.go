35b793d233b32ed184bd67a73e587e33734b8b73
Joonas Loppi <joonas@joonas.fi>
2021-06-21 14:48:23 +0300

    init: add GitHub actions boilerplate

--- cmd/bob/init.go
+++ cmd/bob/init.go
@@ -7,27 +7,33 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/function61/gokit/app/dynversion"
 	"github.com/function61/gokit/os/osutil"
 	"github.com/spf13/cobra"
 )
 
-const (
-	travisFilePath = ".travis.yml"
-	gitlabFilePath = ".gitlab-ci.yml"
-)
+func writeGitHubActionsBoilerplate() error {
+	return writeBoilerplate(".github/workflows/build.yml", `name: Build
+
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - name: Build
+      run: |
+        curl --fail --location --silent --output bob https://function61.com/go/turbobob-latest-linux-amd64 && chmod +x bob
+        CI_REVISION_ID="$GITHUB_SHA" ./bob build --publish-artefacts
+      # unfortunately there doesn't seem to be a way to "expose all secrets", so you must
+      # list here each secret to pass on to the build
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+`)
+}
 
 func writeTravisBoilerplate() error {
-	exists, errExistsCheck := osutil.Exists(travisFilePath)
-	if errExistsCheck != nil {
-		return errExistsCheck
-	}
-
-	if exists {
-		return ErrCiFileAlreadyExists
-	}
-
-	boilerplate := `# Minimal Travis conf for Turbo Bob handoff
+	return writeBoilerplate(".travis.yml", `# Minimal Travis conf for Turbo Bob handoff
 # For help with problems: https://github.com/function61/turbobob/blob/master/docs/ci_travis.md
 
 sudo: required
@@ -36,22 +42,11 @@ language: minimal
 script:
   - curl --fail --location --output bob https://function61.com/go/turbobob-latest-linux-amd64 && chmod +x bob
   - CI_REVISION_ID="$TRAVIS_COMMIT" ./bob build --publish-artefacts
-`
-
-	return ioutil.WriteFile(travisFilePath, []byte(boilerplate), 0600)
+`)
 }
 
 func writeGitLabBoilerplate() error {
-	exists, errExistsCheck := osutil.Exists(gitlabFilePath)
-	if errExistsCheck != nil {
-		return errExistsCheck
-	}
-
-	if exists {
-		return ErrCiFileAlreadyExists
-	}
-
-	boilerplate := `# Minimal Gitlab CI conf for Turbo Bob handoff
+	return writeBoilerplate(".gitlab-ci.yml", `# Minimal Gitlab CI conf for Turbo Bob handoff
 # For help with problems: https://github.com/function61/turbobob/blob/master/docs/ci_gitlab.md
 
 build:
@@ -64,9 +59,29 @@ build:
     - docker:dind
   tags:
     - docker
-`
+`)
+}
 
-	return ioutil.WriteFile(gitlabFilePath, []byte(boilerplate), 0600)
+func writeBoilerplate(filePath string, content string) error {
+	if dir := filepath.Dir(filePath); dir != "" { // make dir if it doesn't exist
+		if err := os.MkdirAll(dir, osutil.FileMode(osutil.OwnerRWX, osutil.GroupRWX, osutil.OtherNone)); err != nil {
+			return err
+		}
+	}
+
+	exists, errExistsCheck := osutil.Exists(filePath)
+	if errExistsCheck != nil {
+		return errExistsCheck
+	}
+
+	if exists {
+		return fmt.Errorf("CI boilerplate '%s' already exists", filePath)
+	}
+
+	return ioutil.WriteFile(
+		filePath,
+		[]byte(content),
+		osutil.FileMode(osutil.OwnerRW, osutil.GroupRW, osutil.OtherNone))
 }
 
 func writeDefaultBobfile(producesDockerImage bool) error {
@@ -124,22 +139,22 @@ func writeDefaultBobfile(producesDockerImage bool) error {
 	return ioutil.WriteFile(
 		bobfileName,
 		[]byte(fmt.Sprintf("%s\n", asJson)),
-		0700)
+		osutil.FileMode(osutil.OwnerRW, osutil.GroupRW, osutil.OtherNone))
 }
 
 func initEntry() *cobra.Command {
+	github := false
 	travis := false
 	gitLab := false
 	docker := false
-	ignoreDevWarning := false
 
 	cmd := &cobra.Command{
 		Use:   "init",
 		Short: "Initializes this project with a default turbobob.json",
 		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			if dynversion.IsDevVersion() && !ignoreDevWarning {
-				osutil.ExitIfError(ErrInitingWithBobDevVersion)
+			if github {
+				osutil.ExitIfError(writeGitHubActionsBoilerplate())
 			}
 
 			if travis {
@@ -154,10 +169,10 @@ func initEntry() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().BoolVarP(&github, "github", "", github, "Write GitHub Actions boilerplate")
 	cmd.Flags().BoolVarP(&travis, "travis", "", travis, "Write Travis CI boilerplate")
 	cmd.Flags().BoolVarP(&gitLab, "gitlab", "", gitLab, "Write GitLab CI boilerplate")
 	cmd.Flags().BoolVarP(&docker, "docker", "", docker, "This project should produce a Docker image?")
-	cmd.Flags().BoolVarP(&ignoreDevWarning, "ignore-dev-warning", "", ignoreDevWarning, "Don't complain about initing with Bob's dev version")
 
 	return cmd
 }
