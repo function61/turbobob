643791349f607a2fbc8b8936c49815448ceb663a
Joonas Loppi <joonas+code@joonas.fi>
2025-01-25 14:29:33 +0200

    `$ bob ws terminal` - launches in focused workspace's project dir

--- cmd/bob/workspace.go
+++ cmd/bob/workspace.go
@@ -8,9 +8,15 @@ package main
 // Currently assumes you're using i3
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
+	"os/exec"
+	"strings"
+	"syscall"
 
+	"github.com/function61/gokit/encoding/jsonfile"
 	"github.com/function61/gokit/os/osutil"
 	"github.com/function61/turbobob/pkg/bobfile"
 	"github.com/spf13/cobra"
@@ -44,6 +50,15 @@ func workspaceEntry() *cobra.Command {
 		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
 			osutil.ExitIfError(workspaceLaunchFileBrowser())
+		},
+	})
+
+	cmd.AddCommand(&cobra.Command{
+		Use:   "terminal [terminal]",
+		Short: "Wrapper for launching a terminal in the currently focused workspace's project directory",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			osutil.ExitIfError(workspaceLaunchTerminal(args[0]))
 		},
 	})
 
@@ -98,17 +113,19 @@ func workspaceLaunchFileBrowser() error {
 }
 
 func workspaceRenameToSelectedProject() error {
+	withErr := func(err error) error { return fmt.Errorf("workspaceRenameToSelectedProject: %w", err) }
+
 	bobfile, err := bobfile.Read()
 	if err != nil {
-		return err
+		return withErr(err)
 	}
 
 	userConfig, err := loadUserconfigFile()
 	if err != nil {
-		return err
+		return withErr(err)
 	}
 
-	nameWithMaybeIcon := func() string {
+	projectNameWithMaybeIcon := func() string {
 		if projectEmojiIcon := bobfile.ProjectEmojiIcon(); projectEmojiIcon != "" && userConfig.WindowManagerShowProjectEmojiIcons {
 			return fmt.Sprintf("%s %s", projectEmojiIcon, bobfile.ProjectName)
 		} else {
@@ -116,5 +133,85 @@ func workspaceRenameToSelectedProject() error {
 		}
 	}()
 
-	return resolveWindowManager().RenameCurrentWorkspace(nameWithMaybeIcon)
+	workdir, err := os.Getwd()
+	if err != nil {
+		return withErr(err)
+	}
+
+	reg, err := readWorkspaceNameRegistry()
+	if err != nil {
+		return withErr(err)
+	}
+	reg[projectNameWithMaybeIcon] = workdir // record to registry
+
+	if err := jsonfile.Write(workspaceNameRegistryPath, reg); err != nil {
+		return withErr(err)
+	}
+	if err := resolveWindowManager().RenameCurrentWorkspace(projectNameWithMaybeIcon); err != nil {
+		return withErr(err)
+	}
+
+	return nil
+}
+
+func workspaceLaunchTerminal(terminal string) error {
+	focusedWorkspace, err := findi3FocusedWorkspace()
+	if err != nil {
+		return err
+	}
+
+	// our window manager interfacing code added workspace number to the workspace name so to match on the name
+	// we need to remove the number first. this is a bit hacky.
+	//
+	prefixToTrim := fmt.Sprintf("%d ", focusedWorkspace.Num)
+	focusedWorkspaceNameWithoutNumber := strings.TrimPrefix(focusedWorkspace.Name, prefixToTrim)
+
+	workspaceNameToProjectRoot, err := readWorkspaceNameRegistry()
+	if err != nil {
+		return err
+	}
+
+	terminalEnvironmentToUse := func() []string {
+		projectRoot, found := workspaceNameToProjectRoot[focusedWorkspaceNameWithoutNumber]
+		// projectRoot, found := "/home/joonas/work/turbobob", true
+		if found { // not found case is ok as well, we simply don't do pwd manipulation
+			if err := os.Chdir(projectRoot); err != nil {
+				panic(err)
+			}
+			// can't do only os.Chdir() here as it manipulates the `PWD` env variable I guess, but `os.Environ()`
+			// is cached and therefore it would not get reflected to `syscall.Exec()`
+			//
+			//TODO: find out why both os.Chdir() and ENV tuning needed!!!
+			return append(os.Environ(), "PWD="+projectRoot)
+		} else {
+			return os.Environ()
+		}
+	}()
+
+	terminalAbsolutePath, err := exec.LookPath(terminal) // exec needs absolute path
+	if err != nil {
+		return err
+	}
+	// using exec to spare one unnecessary hangaround process.
+	// NOTE: this is not expected to return in the happy path.
+	if err := syscall.Exec(terminalAbsolutePath, nil, terminalEnvironmentToUse); err != nil {
+		return fmt.Errorf("exec: %w", err)
+	}
+
+	return nil
+}
+
+const (
+	workspaceNameRegistryPath = "/tmp/bob-workspaces.json"
+)
+
+// maps workspace name => workspace project root
+type workspaceNameRegistry map[string]string
+
+func readWorkspaceNameRegistry() (workspaceNameRegistry, error) {
+	reg := workspaceNameRegistry{}
+	if err := jsonfile.ReadDisallowUnknownFields(workspaceNameRegistryPath, &reg); err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return reg, err
+	}
+	return reg, nil
 }
